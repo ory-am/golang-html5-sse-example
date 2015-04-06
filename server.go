9dac32d64092d161eafd0dab4007d4b8a2397546
arekkas <aenea.rekkas@serlo.org>
2015-04-06 23:41:32 +0200

    Updated example

--- server.go
+++ server.go
@@ -124,33 +124,24 @@ func (b *Broker) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Cache-Control", "no-cache")
 	w.Header().Set("Connection", "keep-alive")
 
-	// Don't close the connection, instead loop 10 times,
-	// sending messages and flushing the response each time
-	// there is a new message to send along.
-	//
-	// NOTE: we could loop endlessly; however, then you
-	// could not easily detect clients that dettach and the
-	// server would continue to send them messages long after
-	// they're gone due to the "keep-alive" header.  One of
-	// the nifty aspects of SSE is that clients automatically
-	// reconnect when they lose their connection.
-	//
-	// A better way to do this is to use the CloseNotifier
-	// interface that will appear in future releases of
-	// Go (this is written as of 1.0.3):
-	// https://code.google.com/p/go/source/detail?name=3292433291b2
-	//
+	// Don't close the connection, instead loop until the
+	// client have closed the conncetion, sending messages
+	// and flushing the response each time there is a new
+	// message to send along.
 	for {
-
-		// Read from our messageChan.
-		msg := <-messageChan
-
+		select {
+		case <-w.(http.CloseNotifier).CloseNotify():
+		// Done
+			log.Println("Finished HTTP request at ", r.URL.Path)
+			return
+		case msg := <-messageChan:
 		// Write to the ResponseWriter, `w`.
-		fmt.Fprintf(w, "data: Message: %s\n\n", msg)
+			fmt.Fprintf(w, "data: Message: %s\n\n", msg)
 
-		// Flush the response.  This is only possible if
-		// the repsonse supports streaming.
-		f.Flush()
+		// Flush the response. This is only possible if
+		// the response supports streaming.
+			f.Flush()
+		}
 	}
 
 	// Done.
